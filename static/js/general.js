5c35cf3fc53aac8eb3878d731c7df834bc4a6b96
Rubén Díaz <outime@gmail.com>
2012-05-04 13:41:20 +0300

    Update static/js/general.js

--- static/js/general.js
+++ static/js/general.js
@@ -8,28 +8,28 @@ function recuperar() {
   
     $('#estado').append('<dt>Comprobando credenciales...</dt>');
 	
-	if (json.galleta)
-	{
-	  galleta = json.galleta;
-	  csfr = json.csfr;
-	  $('#estado').append('<dd><span class="label label-success">OK</span></dd>');
-	  $('#datos').append('<dt>Base</dt> <dd><span class="label">Cookie</span> '+galleta+' <br/> <span class="label">csfr</span> '+csfr+'</dd>');
+      if (json.galleta)
+      {
+        galleta = json.galleta;
+        csfr = json.csfr;
+        $('#estado').append('<dd><span class="label label-success">OK</span></dd>');
+        $('#datos').append('<dt>Base</dt> <dd><span class="label">Cookie</span> '+galleta+' <br/> <span class="label">csfr</span> '+csfr+'</dd>');
 	  
-	  var timerID = setInterval('checkStatus()', 2000);
-	  $('#datos').append('<dt>Progreso</dt> <dd><div class="progress"><div class="bar" style="width: 0%;"></div></div></dd>');
-	  $('#estado').append('<dt>Descargando fotos...</dt>');
+        var timerID = setInterval('checkStatus()', 2000);
+        $('#datos').append('<dt>Progreso</dt> <dd><div class="progress"><div class="bar" style="width: 0%;"></div></div></dd>');
+        $('#estado').append('<dt>Descargando fotos...</dt>');
 	  
-	  $.getJSON('download.php',{step:2,galleta:galleta,csfr:csfr,email:email},function(json) {
-		if (json.status == 'ok') $('#estado').append('<dd><span class="label label-success">OK</span></dd>');
-		$('.bar').attr('style', 'width: 100%');
-		clearInterval(timerID);
+        $.getJSON('download.php',{step:2,galleta:galleta,csfr:csfr,email:email},function(json) {
+          if (json.status == 'ok') $('#estado').append('<dd><span class="label label-success">OK</span></dd>'); 
+          $('.bar').attr('style', 'width: 100%');
+          clearInterval(timerID);
 	  })
-	}
-	else
-	{
-	  $('#estado').append('<dd><span class="label label-important">NOPE</span></dd>');
-	  $('#recuperar').removeAttr('disabled');
-	}
+        }
+        else
+        {
+          $('#estado').append('<dd><span class="label label-important">NOPE</span></dd>');
+          $('#recuperar').removeAttr('disabled');
+        }
   })
 }
 
